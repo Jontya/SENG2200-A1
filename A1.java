660b758b7c2a1537b088413c79a7832e6cd2b3af
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-03-16 20:26:13 +1100

    Renamed scanner obj
    renamed scanner obj for clarity

--- A1.java
+++ A1.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 public class A1 {
     public static void main(String args[]) throws Exception{
         File data = new File(args[0]);
-        Scanner scnr = new Scanner(data);
+        Scanner scanner = new Scanner(data);
 
         MyPolygons l1 = new MyPolygons();
         MyPolygons l2 = new MyPolygons();
 
         String polyData = "";
         
-        while(scnr.hasNext()){
-            String next = scnr.next();
+        while(scanner.hasNext()){
+            String next = scanner.next();
             char ch = next.charAt(0);
-            if(ch == 80 && polyData.length() != 0){
+            if(ch == 80 || ch == 112 && polyData.length() != 0){
                 l1.append(new Polygon(polyData));
                 l2.insertInOrder(new Polygon(polyData));
                 polyData = "";
             }
             else{
-                if(ch != 80){
+                if(ch != 80 && ch != 112){
                     polyData += next + " ";
                 }
-                if(!scnr.hasNext()){
+                if(!scanner.hasNext()){
                     l1.append(new Polygon(polyData));
                     l2.insertInOrder(new Polygon(polyData));
                 }
@@ -34,6 +34,5 @@ public class A1 {
         System.out.print(l1.print());
         System.out.println("Sorted list");
         System.out.print(l2.print());
-
     }
 }
