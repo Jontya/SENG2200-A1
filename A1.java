3a8a1bff562700b3fec8d75d347c50c6552a94ab
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-03-16 20:59:47 +1100

    Code Commented

--- A1.java
+++ A1.java
@@ -6,30 +6,31 @@ public class A1 {
         File data = new File(args[0]);
         Scanner scanner = new Scanner(data);
 
-        MyPolygons l1 = new MyPolygons();
-        MyPolygons l2 = new MyPolygons();
+        MyPolygons l1 = new MyPolygons(); // Unsorted List
+        MyPolygons l2 = new MyPolygons(); // Sorted List
 
-        String polyData = "";
+        String polyData = ""; // Each polygon string is stored here
         
-        while(scanner.hasNext()){
-            String next = scanner.next();
+        while(scanner.hasNext()){ // While file still has content
+            String next = scanner.next(); // Next token is stored
             char ch = next.charAt(0);
-            if(ch == 80 || ch == 112 && polyData.length() != 0){
-                l1.append(new Polygon(polyData));
+            if(ch == 80 && polyData.length() != 0){ // Checks for a "P"
+                l1.append(new Polygon(polyData)); // Adds the previous data to both lists
                 l2.insertInOrder(new Polygon(polyData));
                 polyData = "";
             }
             else{
-                if(ch != 80 && ch != 112){
-                    polyData += next + " ";
+                if(ch != 80){
+                    polyData += next + " "; // Adds next token to string
                 }
-                if(!scanner.hasNext()){
-                    l1.append(new Polygon(polyData));
+                if(!scanner.hasNext()){ // Checks for the last token
+                    l1.append(new Polygon(polyData)); // Adds last token to both lists
                     l2.insertInOrder(new Polygon(polyData));
                 }
             }
         }
         
+        // Prints output to console
         System.out.println("Unsorted list");
         System.out.print(l1.print());
         System.out.println("Sorted list");
