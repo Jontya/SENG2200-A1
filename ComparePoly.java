6c4b4210723ef029775ee2b7e499d143ca3f70d9
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-03-17 13:25:16 +1100

    Moved Interface into its own file

---
+++ ComparePoly.java
@@ -0,0 +1,5 @@
+
+// The interface implemented inside of the Polygon class to compare two polygons and determine if this.Polygon should come before the param
+public interface ComparePoly { 
+    boolean comesBefore(Polygon o);
+}
