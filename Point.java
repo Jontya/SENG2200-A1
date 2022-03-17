e646991f5a763e89bcf692301281cdd34d6be938
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-03-17 13:26:16 +1100

    Changed Method Naming

--- Point.java
+++ Point.java
@@ -1,28 +1,36 @@
 import java.lang.Math;
 public class Point {
 
-    private double xcord;
-    private double ycord;
+    private double x;
+    private double y;
 
-    public Point(double x, double y){
-        xcord = x;
-        ycord = y;
+    public Point(double _x, double _y){
+        x = _x;
+        y = _y;
     }
 
     public double distanceFromOrigin(){ // Calculates a points distance from (0, 0)
-        return Math.sqrt(Math.pow((xcord - 0), 2) + Math.pow((ycord - 0), 2));
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
-    public String coordinatesString(){ // Clean string for a point "(x, y)"
-        return "(" + String.format("%.2f", xcord) + " , " + String.format("%.2f", ycord) + ")" ;
+    public String toString(){ // Clean string for a point "(x, y)"
+        return "(" + String.format("%4.2f", x) + " , " + String.format("%4.2f", y) + ")" ;
     }
 
     public double getx(){
-        return xcord;
+        return x;
     }
 
     public double gety(){
-        return ycord;
+        return y;
+    }
+
+    public void setx(double _x){
+        x = _x;
+    }
+
+    public void sety(double _y){
+        y = _y;
     }
     
 }
